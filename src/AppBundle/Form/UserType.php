cb7b7cac9f2f64719df33493df619b334c5e8ee5
Dirk Kuijer <dirk@feka.nl>
2019-07-18 08:32:18 +0200

    874 files?

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $user = $builder->getData();
+
+        $builder
+            ->add('email', EmailType::class, ['label' => 'form.email',
+                'attr' => ['placeholder' => 'naam@mail.nl'],
+                'translation_domain' => 'FOSUserBundle', ])
+
+            ->add('username', null, ['label' => 'form.username',
+                'translation_domain' => 'FOSUserBundle', ])
+
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'expanded' => true,
+                'placeholder' => 'false',
+                'choices' => [
+                    'Gebruiker' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+            ])
+        ;
+
+        if (null == $user->getId()) {
+            $builder
+                ->add('plainPassword', RepeatedType::class, [
+                    'type' => PasswordType::class,
+                    'options' => [
+                        'translation_domain' => 'FOSUserBundle',
+                        'attr' => [
+                            'autocomplete' => 'new-password',
+                        ],
+                    ],
+                    'first_options' => ['label' => 'form.password',
+                        'attr' => ['placeholder' => 'Minimaal 10 karakters',
+                        ], ],
+                    'second_options' => ['label' => 'form.password_confirmation',
+                        'attr' => ['placeholder' => 'Minimaal 10 karakters',
+                        ], ],
+                    'invalid_message' => 'fos_user.password.mismatch',
+                ])
+            ;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\User',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+}
